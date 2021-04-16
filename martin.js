799ffc2975672730f43db8ecb307f92945a88cfc
marlokalle <74901014+marlokalle@users.noreply.github.com>
2021-04-16 10:18:42 +0200

    update - martins nye js

--- martin.js
+++ martin.js
@@ -1,120 +1,30 @@
-const startButton = document.getElementById('start-btn')
-const nextButton = document.getElementById('next-btn')
-const questionContainerElement = document.getElementById('question-container')
-const questionElement = document.getElementById('question')
-const answerButtonsElement = document.getElementById('answer-buttons')
+// card flip kode
 
-let shuffledQuestions, currentQuestionIndex
 
-startButton.addEventListener('click', startGame)
-nextButton.addEventListener('click', () => {
-  currentQuestionIndex++
-  setNextQuestion()
-})
+document.addEventListener('DOMContentLoaded', function(event) {
 
-function startGame() {
-  startButton.classList.add('hide')
-  shuffledQuestions = questions.sort(() => 1)
-  currentQuestionIndex = 0
-  questionContainerElement.classList.remove('hide')
-  setNextQuestion()
-}
+document.getElementById('flip-card-btn-turn-to-back').style.visibility = 'visible';
+document.getElementById('flip-card-btn-turn-to-back2').style.visibility = 'visible';
+document.getElementById('flip-card-btn-turn-to-back3').style.visibility = 'visible';
+document.getElementById('flip-card-btn-turn-to-front').style.visibility = 'hidden';
+document.getElementById('flip-card-btn-turn-to-front2').style.visibility = 'hidden';
+document.getElementById('flip-card-btn-turn-to-front3').style.visibility = 'hidden';
 
-function setNextQuestion() {
-  resetState()
-  showQuestion(shuffledQuestions[currentQuestionIndex])
-}
+document.getElementById('flip-card-btn-turn-to-back').onclick = function() {
+  document.getElementById("flip-card-btn-turn-to-back2").disabled = true;
+    document.getElementById("flip-card-btn-turn-to-back3").disabled = true;
+document.getElementById('flip-card').classList.toggle('do-flip');
+};
+document.getElementById('flip-card-btn-turn-to-back2').onclick = function() {
+  document.getElementById("flip-card-btn-turn-to-back3").disabled = true;
+    document.getElementById("flip-card-btn-turn-to-back").disabled = true;
+document.getElementById('flip-card2').classList.toggle('do-flip');
+};
+document.getElementById('flip-card-btn-turn-to-back3').onclick = function() {
+  document.getElementById("flip-card-btn-turn-to-back2").disabled = true;
+    document.getElementById("flip-card-btn-turn-to-back").disabled = true;
+document.getElementById('flip-card3').classList.toggle('do-flip');
+};
 
-function showQuestion(question) {
-  questionElement.innerText = question.question
-  question.answers.forEach(answer => {
-    const button = document.createElement('button')
-    button.innerText = answer.text
-    button.classList.add('btn')
-    if (answer.correct) {
-      button.dataset.correct = answer.correct
-    }
-    button.addEventListener('click', selectAnswer)
-    answerButtonsElement.appendChild(button)
-  })
-}
-
-function resetState() {
-  clearStatusClass(document.body)
-  nextButton.classList.add('hide')
-  while (answerButtonsElement.firstChild) {
-    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
-  }
-}
-
-function selectAnswer(e) {
-  const selectedButton = e.target
-  const correct = selectedButton.dataset.correct
-  setStatusClass(document.body, correct)
-  Array.from(answerButtonsElement.children).forEach(button => {
-    setStatusClass(button, button.dataset.correct)
-  })
-  if (shuffledQuestions.length > currentQuestionIndex + 1) {
-    nextButton.classList.remove('hide')
-  } else {
-    startButton.innerText = 'Afslut quiz'
-    startButton.classList.remove('hide')
-  }
-}
-
-function setStatusClass(element, correct) {
-  clearStatusClass(element)
-  if (correct) {
-    element.classList.add('correct')
-  } else {
-    element.classList.add('wrong')
-  }
-}
-
-function clearStatusClass(element) {
-  element.classList.remove('correct')
-  element.classList.remove('wrong')
-}
-
-const questions = [
-  {
-    question: 'Hvad er ensomhed?',
-    answers: [
-      { text: 'En følelse, der opstår når man ikke får opfyldt sit sociale behov', correct: true },
-      { text: 'En følelse, der opstår, når man er bange', correct: false },
-      { text: 'En følelse, der opstår, når man keder sig', correct: false }
-    ]
-  },
-  {
-    question: 'Kan man være ensom, selvom man er omkredset af mennesker?',
-    answers: [
-      { text: 'Ja, for man kan sagtens føle sig alene i selskab med andre', correct: true },
-      { text: 'Nej, selvfølgelig er man ikke ensom i andres selskab', correct: false },
-      { text: 'Nej, hvis du er ensom kender du ingen mennesker', correct: false }
-    ]
-  },
-  {
-    question: 'Hvor mange unge føler sig ensomme i Danmark?',
-    answers: [
-      { text: '15.000', correct: false },
-      { text: '125.000', correct: true },
-      { text: '50.000', correct: false }
-    ]
-  },
-  {
-    question: 'Hvilke konsekvenser kan forekomme af langvarig ensomhed?',
-    answers: [
-      { text: 'Dårligere søvn og hovedpine', correct: false },
-      { text: 'Depression og mindre empatisk', correct: false },
-      { text: 'Alle tingene', correct: true }
-    ]
-  },
-  {
-    question: 'Hvad kan du gøre, hvis du føler dig ensom?',
-    answers: [
-      { text: 'Ingenting og håbe på at det bliver bedre', correct: false },
-      { text: 'Samle mod til at snakke med nogen, eller søge hjælp', correct: true },
-      { text: 'Spørge din huns til råds', correct: false }
-    ]
-  }
-]
+});
+// her stopper card flip koden
