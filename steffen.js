885978f53e6d9602ac99a272436e028c75934590
loevgreen <s.loevgreen@gmail.com>
2021-04-17 19:17:08 +0200

    Dropdown

--- steffen.js
+++ steffen.js
@@ -1,6 +1,5 @@
 var btn = document.getElementById('nextbtn-korrekt');
 var bar = document.getElementById('bar');
-
 var count = 0;
 
 btn.addEventListener('click', ()=>{
@@ -12,3 +11,12 @@ btn.addEventListener('click', ()=>{
       count = count + 20;
   }
 });
+
+function expand() {
+  document.getElementById("expand").style.display = "block";
+  document.getElementById("expand").style.height = "80vh";
+  document.getElementById("progressbar-container").style.display = "none";
+  document.getElementById("flip-card").style.display = "none";
+  document.getElementById("flip-card2").style.display = "none";
+  document.getElementById("flip-card3").style.display = "none";
+}
