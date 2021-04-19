e0ad2a6b78fbe300606bbb9be2ace11f7643e139
loevgreen <s.loevgreen@gmail.com>
2021-04-19 15:18:50 +0200

    Overflow

--- steffen.js
+++ steffen.js
@@ -1,6 +1,8 @@
 var btn = document.getElementById('nextbtn-korrekt');
 var bar = document.getElementById('bar');
+
 var count = 0;
+
 
 btn.addEventListener('click', ()=>{
   bar.style.width = count + '%';
@@ -13,10 +15,12 @@ btn.addEventListener('click', ()=>{
 });
 
 function expand() {
+  document.getElementById("expand").style.visibility = "visible";
   document.getElementById("expand").style.display = "block";
   document.getElementById("expand").style.height = "80vh";
   document.getElementById("progressbar-container").style.display = "none";
   document.getElementById("flip-card").style.display = "none";
   document.getElementById("flip-card2").style.display = "none";
   document.getElementById("flip-card3").style.display = "none";
+  document.getElementById("question-container").style.display = "none";
 }
